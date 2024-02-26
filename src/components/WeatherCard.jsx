e3036da228822cf22ea08bba00170af351e66d91
Kamisia <kamila.samczuk@gmail.com>
2024-02-26 15:20:40 +0100

    modified:   src/components/WeatherCard.jsx 	modified:   src/index.css

--- src/components/WeatherCard.jsx
+++ src/components/WeatherCard.jsx
@@ -1,5 +1,11 @@
 import { useGlobalContext } from "../Context";
 import { useQuery } from "react-query";
+import { GiWindsock } from "react-icons/gi";
+import { WiThermometer } from "react-icons/wi";
+import { WiHumidity } from "react-icons/wi";
+import { WiUmbrella } from "react-icons/wi";
+import { WiBarometer } from "react-icons/wi";
+
 const WeatherCard = () => {
   const { fetchCurrentWeather } = useGlobalContext();
 
@@ -17,12 +23,29 @@ const WeatherCard = () => {
 
   return (
     <div className="single-card">
+      <p className="time">{data.location.localtime}</p>
       <h1>{data.location.name}</h1>
+      <img src={data.current.condition.icon} />
       <div className="info-container">
-        <></>
-        <p>: {data.current.temp_c}°C</p>
-        <p>Condition: {data.current.condition.text}</p>
-        <img src={data.current.condition.icon} />
+        <p className="temp">
+          <WiThermometer /> {data.current.temp_c}°C
+        </p>
+        <div className="wind-info">
+          <p className="temp">
+            <GiWindsock />
+            {data.current.wind_kph} km/h {data.current.wind_dir}
+          </p>
+          <p>
+            <WiBarometer /> {data.current.pressure_mb}hPa
+          </p>
+          <p>
+            <WiUmbrella /> {data.current.precip_mm} mm
+          </p>
+          <p>
+            <WiHumidity />
+            {data.current.humidity}%
+          </p>
+        </div>
       </div>
     </div>
   );
